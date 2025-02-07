bc1eae5593dd0e0cf9c955ac6224aa2dd6ea35cf
JISUNG PARK <meowbutlerdev@gmail.com>
2025-02-07 14:19:02 +0900

    feat: 숫자 레이블 및 버튼 기능 구현
    - 현재 숫자 표시 레이블
    - "+" 버튼 기능
    - "-" 버튼 기능
    - 리셋 버튼 기능

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        count += 1
+        numberLabel.text = String(count)
+    }
+    
+    @IBAction func minusButtonTapped(_ sender: UIButton) {
+        count -= 1
+        numberLabel.text = String(count)
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        count = 0
+        numberLabel.text = String(count)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
